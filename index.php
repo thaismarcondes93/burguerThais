5217c6ae57fec705cd30f24ebf30b98d60e2e89a
thaismarcondes93 <thais.marcondes@outlook.com>
2021-03-04 15:17:09 -0300

    Redimensionar

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ O melhor hamburguer de SP since 2015<br>
 Promoção 2 hamburguers por 50,00<br>
 <img src="https://blog.castellmaq.com.br/wp-content/uploads/2020/05/como-abrir-uma-hamburgueria.jpg" style="
     width: 254px;
-">>
+">
